2556216cbe5ab143095f1627f491ce71e728e53b
Frontenddeveloper2023 <Jayamweb.designer2@gmail.com>
2026-06-23 13:56:07 +0530

    gst backend

---
+++ src/api/gstApi.js
@@ -0,0 +1,21 @@
+import api from './api'; 
+
+export const createGstAPI = async (gstData) => {
+  const response = await api.post('/api/gst/create-gst', gstData);
+  return response.data;
+};
+
+export const getAllGstAPI = async () => {
+  const response = await api.get('/api/gst/getAll-gst');
+  return response.data;
+};
+
+export const updateGstAPI = async (id, gstData) => {
+  const response = await api.put(`/api/gst/update-gst/${id}`, gstData);
+  return response.data;
+};
+
+export const deleteGstAPI = async (id) => {
+  const response = await api.put(`/api/gst/delete-gst/${id}`);
+  return response.data;
+};
